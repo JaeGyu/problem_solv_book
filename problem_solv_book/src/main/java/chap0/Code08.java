6acbf12dbbb351d3b39b18f5251caabb568c893b
jpmlyr <jpmlyr@gmail.com>
2016-02-19 00:47:26 +0900

    0.3 생각해보기 까지.

--- problem_solv_book/src/main/java/chap0/Code08.java
+++ problem_solv_book/src/main/java/chap0/Code08.java
@@ -18,12 +18,63 @@ public class Code08 {
 		arr[s] = last;
 	}
 
-	
+	public void left_rotate(int[] arr, int s, int t) {
+		int first = arr[s];
+
+		for (int i = s; i < t; i++) {
+			arr[i] = arr[i + 1];
+		}
+		arr[t] = first;
+	}
+
+	public void right_roate_N(int[] arr, int s, int t, int n) {
+		int cnt = t - s + 1;
+		int toIndexingNum = n % cnt;
+		int[] temp = Arrays.copyOf(arr, arr.length);
+
+		for (int i = s; i <= t; i++) {
+			arr[(i + toIndexingNum > t) ? toIndexingNum + i - t : toIndexingNum
+					+ i] = temp[i];
+		}
+
+	}
+
 	@Test
 	public void testRight_rotate() throws Exception {
-		int[] arr = {1,2,3,4,5,6,7,8};
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
 		this.right_rotate(arr, 2, 6);
-		int[] expectArr = {1,2,7,3,4,5,6,8};
-	    assertArrayEquals(expectArr, arr);
-	} 
+		int[] expectArr = { 1, 2, 7, 3, 4, 5, 6, 8 };
+		assertArrayEquals(expectArr, arr);
+	}
+
+	@Test
+	public void testLeft_rotate() throws Exception {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] expectArr = { 1, 3, 4, 5, 6, 7, 2, 8 };
+		this.left_rotate(arr, 1, 6);
+
+		assertArrayEquals(expectArr, arr);
+
+	}
+
+	@Test
+	public void testRightRotateN() throws Exception {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] expectArr = { 1, 3, 2, 4, 5, 6, 7, 8 };
+
+		this.right_roate_N(arr, 1, 2, 3);
+
+		assertArrayEquals(expectArr, arr);
+	}
+
+	@Test
+	public void testRightRotateN_2() throws Exception {
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] expectArr = { 1, 3, 4, 5, 2, 6, 7, 8 };
+
+		this.right_roate_N(arr, 1, 4, 3);
+
+		assertArrayEquals(expectArr, arr);
+	}
+
 }
